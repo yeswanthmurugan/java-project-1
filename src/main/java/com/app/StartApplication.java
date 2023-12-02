d04aaa566ea77f2513c17f76bfc8ac97c027758b
cupcy-cloud <thiyagarajan8606@gmail.com>
2023-12-02 19:57:56 +0530

    added java project

---
+++ src/main/java/com/app/StartApplication.java
@@ -0,0 +1,24 @@
+package com.abhishek;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@SpringBootApplication
+@Controller
+public class StartApplication {
+
+    @GetMapping("/")
+    public String index(final Model model) {
+        model.addAttribute("title", "Welcome to the CUPCY CLOUD DevOps World!!!");
+        model.addAttribute("msg", "This is sample Application. Thanks Buddy!!!");
+        return "index";
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(StartApplication.class, args);
+    }
+
+}
