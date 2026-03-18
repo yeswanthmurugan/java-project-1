71f8d6eea1558f67b5cb45f20e55a1f7fe92794f
yeswanthmurugan <yeswanthmurugan3115@gmail.com>
2026-03-18 15:02:16 +0530

    changed java file

--- src/main/java/com/app/StartApplication.java
+++ src/main/java/com/app/StartApplication.java
@@ -12,7 +12,7 @@ public class StartApplication {
 
     @GetMapping("/")
     public String index(final Model model) {
-        model.addAttribute("title", "Welcome to the CUPCY CLOUD DevOps TECH Changes........");
+        model.addAttribute("title", "Welcome sabari to devops leraning........");
         model.addAttribute("msg", "This is sample Application. Thanks!!!");
         return "index";
     }
